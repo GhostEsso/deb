d33a3a7c90b226c30f6b87db6208b713d25d593b
Essohanam TAMBANA <philippe.tambana@kaba-delivery.com>
2026-01-27 11:10:09 +0000

    Migration: Prepare API for Vercel deployment

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Express, Request, Response } from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let cachedApp: any;
+
+async function bootstrap(): Promise<Express> {
+  const server: Express = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors();
   app.setGlobalPrefix('api/V1');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
+  return server;
 }
-bootstrap();
+
+const handler = async (req: Request, res: Response) => {
+  if (!cachedApp) {
+    cachedApp = await bootstrap();
+  }
+  return cachedApp(req, res);
+};
+
+// Pour le développement local
+if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+  const port = process.env.PORT ?? 3000;
+  bootstrap().then((server) => {
+    server.listen(port, () => {
+      console.log(`Application is running on: http://localhost:${port}/api/V1`);
+    });
+  });
+}
+
+export default handler;

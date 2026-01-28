e45d35fe3157a8563dadd9f7a7bb4ff169dcc712
Essohanam TAMBANA <philippe.tambana@kaba-delivery.com>
2026-01-28 07:16:10 +0000

    fix(api): fix imports by adding .js extension for nodenext compatibility

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
-import { PrismaModule } from '../prisma/prisma.module';
-import { CommonModule } from '../common/common.module';
+import { UsersController } from './users.controller.js';
+import { UsersService } from './users.service.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
+import { CommonModule } from '../common/common.module.js';
 
 import { MulterModule } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';

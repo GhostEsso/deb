6ffff68323a8d4f98a4a323fec4eee9b839f0e64
Essohanam TAMBANA <philippe.tambana@kaba-delivery.com>
2026-01-28 07:34:19 +0000

    fix(api): revert to commonjs and simplify vercel config to fix build failures

--- api/src/stories/stories.module.ts
+++ api/src/stories/stories.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { StoriesService } from './stories.service.js';
-import { StoriesController } from './stories.controller.js';
-import { PrismaModule } from '../prisma/prisma.module.js';
-import { CommonModule } from '../common/common.module.js';
+import { StoriesService } from './stories.service';
+import { StoriesController } from './stories.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { CommonModule } from '../common/common.module';
 
 import { MulterModule } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
